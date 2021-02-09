5e0faa34a7f00ce09f403b9a3eda4b1555823aef
Mohammad Hossein Golestan <mohammad.h.golestan@ntnu.no>
2021-02-09 10:11:24 +0100

    2.14

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,13 +1,53 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+const Weather = ({ city }) => {
+  const api_key = process.env.REACT_APP_API_KEY;
+  const [cityData, setCityData] = useState({
+    temperature: 0,
+    icon: "",
+    wind_speed: 0,
+    wind_direction: 0,
+  });
+
+  useEffect(
+    () =>
+      axios
+        .get(
+          `http://api.weatherstack.com/current?access_key=${api_key}&query=${city}`
+        )
+        .then((response) => {
+          const newCityData = {
+            temeperature: response.data.current.temperature,
+            icon: response.data.current.weather_icons,
+            wind_speed: response.data.current.wind_speed,
+            wind_direction: response.data.current.wind_dir,
+          };
+
+          setCityData(newCityData);
+        }),
+    []
+  );
+  return (
+    <div>
+      <h2>Weather in {city}</h2>
+      <p>Temperature: {cityData.temeperature}</p>
+      <img str={cityData.icon} alt="icon"></img>
+      <p>
+        <strong>Wind</strong> {cityData.wind_speed} mph direction{" "}
+        {cityData.wind_direction}
+      </p>
+    </div>
+  );
+};
+
 const CountriesList = ({ countriesToShow, setFilteredCountries }) => {
   const handleClick = (countryName) => {
     setFilteredCountries(countryName);
   };
 
-  if (countriesToShow.length === 0) {
-    return <div></div>;
+  if (countriesToShow.length === 250) {
+    return <div>Insert the name of the country</div>;
   } else if (countriesToShow.length === 1) {
     return (
       <div>
@@ -21,6 +61,7 @@ const CountriesList = ({ countriesToShow, setFilteredCountries }) => {
           ))}
         </ul>
         <img src={countriesToShow[0].flag} alt="flag" />
+        <Weather city={countriesToShow[0].capital} />
       </div>
     );
   } else if (countriesToShow.length > 10) {
